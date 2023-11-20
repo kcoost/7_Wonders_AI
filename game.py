fb333831f7a4820cce39d84ff40d40b87eabc36e
koen <koen@oostermeijer.net>
2023-11-20 22:29:52 +0000

    rename tableau to board

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+
 random.seed(0)
 
 from common import *
@@ -7,142 +8,196 @@ from players import *
 import logger
 from players.policy import StupidAI
 
+
 class GameState:
-	def __init__(self, players):
+    def __init__(self, players):
+        self.player_count = len(players)
+        self.players = []
+        self.ages = []
+        self.decks = [[]] * len(players)
+        self.discard_pile = []
+        self.logger = logger.Logger()
+        for i in range(len(players)):
+            name, persona = players[i]
+            self.players.append(Players.Player(name))
+            self.players[i].set_personality(persona())
 
-		self.player_count = len(players)
-		self.players = []
-		self.ages = []
-		self.decks = [[]] * len(players)
-		self.discard_pile = []
-		self.logger = logger.Logger()
-		for i in range(len(players)):
-			name, persona = players[i]
-			self.players.append(Players.Player(name))
-			self.players[i].set_personality(persona())
+        cards = helpers.read_cards_file("card-descriptions.txt")
+        self.setup_age_cards(cards)
 
-		cards = helpers.read_cards_file("card-descriptions.txt")
-		self.setup_age_cards(cards)
+    def setup_age_cards(self, cards):
+        self.logger.card_list = cards
+        age_1 = [c for c in cards if c.age == 1 and c.players <= self.player_count]
+        age_2 = [c for c in cards if c.age == 2 and c.players <= self.player_count]
+        age_3 = [
+            c
+            for c in cards
+            if c.age == 3
+            and c.get_colour() != CARDS_PURPLE
+            and c.players <= self.player_count
+        ]
+        purple = [
+            c
+            for c in cards
+            if c.age == 3
+            and c.get_colour() == CARDS_PURPLE
+            and c.players <= self.player_count
+        ]
 
-	def setup_age_cards(self, cards):
-		self.logger.card_list = cards
-		age_1 = [c for c in cards if c.age == 1 and c.players <= self.player_count]
-		age_2 = [c for c in cards if c.age == 2 and c.players <= self.player_count]
-		age_3 = [c for c in cards if c.age == 3 and c.get_colour() != CARDS_PURPLE and c.players <= self.player_count]
-		purple = [c for c in cards if c.age == 3 and c.get_colour() == CARDS_PURPLE and c.players <= self.player_count]
+        random.shuffle(age_1)
+        random.shuffle(age_2)
+        random.shuffle(purple)
+        age_3 += purple[0 : self.player_count + 2]
+        random.shuffle(age_3)
 
-		random.shuffle(age_1)
-		random.shuffle(age_2)
-		random.shuffle(purple)
-		age_3 += purple[0 : self.player_count + 2]
-		random.shuffle(age_3)
+        self.ages = [age_1, age_2, age_3]
 
-		self.ages = [age_1, age_2, age_3]
+    def deal_age_cards(self, age):
+        cards = self.ages[age][0:]
+        p = 0
+        for i in range(self.player_count):
+            self.decks[i] = []
+        while len(cards):
+            self.decks[p].append(cards[0])
+            p += 1
+            p %= self.player_count
+            cards = cards[1:]
 
-	def deal_age_cards(self, age):
-		cards = self.ages[age][0:]
-		p = 0
-		for i in range(self.player_count):
-			self.decks[i] = []
-		while len(cards):
-			self.decks[p].append(cards[0])
-			p += 1
-			p %= self.player_count
-			cards = cards[1:]
+    def _get_west_player(self, playerid):
+        return self.players[(playerid + self.player_count - 1) % self.player_count]
 
-	def _get_west_player(self, playerid):
-		return self.players[(playerid + self.player_count - 1) % self.player_count]
+    def _get_east_player(self, playerid):
+        return self.players[(playerid + 1) % self.player_count]
 
-	def _get_east_player(self, playerid):
-		return self.players[(playerid + 1) % self.player_count]
+    def play_turn(self, offset):
+        for i in range(self.player_count):
+            player = self.players[i]
+            west_player = self._get_west_player(i)
+            east_player = self._get_east_player(i)
+            deckid = (i + offset) % self.player_count
+            # This loop is actually wrong.
+            # Everyone should choose the card they will play, server
+            # validates the move is legal, then each player plays the card
+            # Then each player adds the new card to their board
+            while True:
+                action, card = player.play_hand(
+                    self.decks[deckid], west_player, east_player
+                )
+                if action == ACTION_PLAYCARD:
+                    can_buy = False
+                    # see if the player can buy the card
+                    if player.can_build_with_chain(card):
+                        can_buy = True
+                        self.logger.log_buy_card_with_chain(player, card)
+                    else:
+                        buy_options = player.buy_card(card, west_player, east_player)
+                        if len(buy_options) == 0:
+                            continue
+                        self.logger.log_buy_card(player, card, buy_options[0])
+                        can_buy = True
+                        player.money -= buy_options[0].total_cost
+                        east_player.money += buy_options[0].east_cost
+                        west_player.money += buy_options[0].west_cost
+                    if can_buy:
+                        card.play(player, west_player, east_player)
+                        player.get_cards().append(card)
+                        break
+                elif action == ACTION_DISCARD:
+                    self.logger.log_action(player, action, card)
+                    self.discard_pile.append(card)
+                    player.money += 3
+                    break
+                elif action == ACTION_STAGEWONDER:
+                    self.logger.log_action(player, action, card)
+                    # make sure we can do that
+                    break
+            self.decks[deckid].remove(card)
 
-	def play_turn(self, offset):
-		for i in range(self.player_count):
-			player = self.players[i]
-			west_player = self._get_west_player(i)
-			east_player = self._get_east_player(i)
-			deckid = (i + offset) % self.player_count
-			# This loop is actually wrong.
-			# Everyone should choose the card they will play, server
-			# validates the move is legal, then each player plays the card
-			# Then each player adds the new card to their tableau
-			while True:
-				action, card = player.play_hand(self.decks[deckid], west_player, east_player)
-				if action == ACTION_PLAYCARD:
-					can_buy = False
-					# see if the player can buy the card
-					if player.can_build_with_chain(card):
-						can_buy = True
-						self.logger.log_buy_card_with_chain(player, card)
-					else:
-						buy_options = player.buy_card(card, west_player, east_player)
-						if len(buy_options) == 0:
-							continue
-						self.logger.log_buy_card(player, card, buy_options[0])
-						can_buy = True
-						player.money -= buy_options[0].total_cost
-						east_player.money += buy_options[0].east_cost
-						west_player.money += buy_options[0].west_cost
-					if can_buy:
-						card.play(player, west_player, east_player)
-						player.get_cards().append(card)
-						break
-				elif action == ACTION_DISCARD:
-					self.logger.log_action(player, action, card)
-					self.discard_pile.append(card)
-					player.money += 3
-					break
-				elif action == ACTION_STAGEWONDER:
-					self.logger.log_action(player, action, card)
-					# make sure we can do that
-					break
-			self.decks[deckid].remove(card)
+    def game_loop(self):
+        for age in range(3):
+            self.logger.log_age_header(age)
+            self.deal_age_cards(age)
+            offset = 0
+            while len(self.decks[0]) > 1:
+                self.play_turn(offset)
+                offset = (
+                    offset + [1, self.player_count - 1, 1][age]
+                ) % self.player_count
+            # everyone discards the last card
+            for p in range(self.player_count):
+                self.discard_pile.append(self.decks[p][0])
 
+            # score military
+            for p in range(self.player_count):
+                west = self._get_west_player(p)
+                east = self._get_east_player(p)
+                player = self.players[p]
+                player_strength, opponent_strength, score = helpers.score_military(
+                    player, west, age
+                )
+                self.logger.log_military_battle(
+                    player.get_name(),
+                    player_strength,
+                    west.get_name(),
+                    opponent_strength,
+                    score,
+                )
+                self.players[p].military.append(score)
+                player_strength, opponent_strength, score = helpers.score_military(
+                    player, east, age
+                )
+                self.logger.log_military_battle(
+                    player.get_name(),
+                    player_strength,
+                    east.get_name(),
+                    opponent_strength,
+                    score,
+                )
+                self.players[p].military.append(score)
+        for i in range(self.player_count):
+            player = self.players[i]
+            west = self._get_west_player(i)
+            east = self._get_east_player(i)
+            score = 0
+            bluescore = helpers.score_blue(player)
+            (
+                _,
+                _,
+                _,
+            ), greenscore = helpers.score_science(player)
+            score += greenscore
+            redscore = 0
+            for military in player.military:
+                redscore += military
+            moneyscore = player.money / 3
+            yellowscore = helpers.score_yellow(player, west, east)
+            purplescore = helpers.score_purple(player, west, east)
+            totalscore = (
+                bluescore
+                + greenscore
+                + redscore
+                + moneyscore
+                + yellowscore
+                + purplescore
+            )
+            text = (
+                "Final score: Blue: %d, Green: %d, red: %d, yellow: %d, purple: %d, $: %d, total: %d"
+                % (
+                    bluescore,
+                    greenscore,
+                    redscore,
+                    yellowscore,
+                    purplescore,
+                    moneyscore,
+                    totalscore,
+                )
+            )
+            self.logger.log_freetext(player.get_name() + " " + text)
 
-	def game_loop(self):
-		for age in range(3):
-			self.logger.log_age_header(age)
-			self.deal_age_cards(age)
-			offset = 0
-			while len(self.decks[0]) > 1:
-				self.play_turn(offset)
-				offset = (offset + [1, self.player_count - 1, 1][age]) % self.player_count
-			# everyone discards the last card
-			for p in range(self.player_count):
-				self.discard_pile.append(self.decks[p][0])
+        logfile = open("logfile.txt", "w")
+        self.logger.dump(logfile)
+        logfile.close()
 
-			# score military
-			for p in range(self.player_count):
-				west = self._get_west_player(p)
-				east = self._get_east_player(p)
-				player = self.players[p]
-				player_strength, opponent_strength, score = helpers.score_military(player, west, age)
-				self.logger.log_military_battle(player.get_name(), player_strength, west.get_name(), opponent_strength, score)
-				self.players[p].military.append(score)
-				player_strength, opponent_strength, score = helpers.score_military(player, east, age)
-				self.logger.log_military_battle(player.get_name(), player_strength, east.get_name(), opponent_strength, score)
-				self.players[p].military.append(score)
-		for i in range(self.player_count):
-			player = self.players[i]
-			west = self._get_west_player(i)
-			east = self._get_east_player(i)
-			score = 0
-			bluescore = helpers.score_blue(player)
-			(_,_,_,), greenscore = helpers.score_science(player)
-			score += greenscore
-			redscore = 0
-			for military in player.military:
-				redscore += military
-			moneyscore = player.money / 3
-			yellowscore = helpers.score_yellow(player, west, east)
-			purplescore = helpers.score_purple(player, west, east)
-			totalscore = bluescore + greenscore + redscore + moneyscore + yellowscore + purplescore
-			text = "Final score: Blue: %d, Green: %d, red: %d, yellow: %d, purple: %d, $: %d, total: %d" % (bluescore, greenscore, redscore, yellowscore, purplescore, moneyscore, totalscore)
-			self.logger.log_freetext(player.get_name() + " " + text)
-
-		logfile = open("logfile.txt", "w")
-		self.logger.dump(logfile)
-		logfile.close()
 
 # Alice = Personalities.StupidAI("Alice")
 # Bob = Personalities.StupidAI("Bob")
@@ -152,9 +207,8 @@ game.game_loop()
 
 # test
 with open("logfile_reference.txt") as f:
-	reference_game = f.read()
+    reference_game = f.read()
 with open("logfile.txt") as f:
-	game = f.read()
+    game = f.read()
 
 assert reference_game == game, "Game results differ from previous version"
-
--- players/Players.py
+++ players/Players.py
@@ -2,197 +2,248 @@ from collections import deque
 from common import *
 import cards
 
+
 class Player:
-	def __init__(self, name):
-		self.name = name
-		self.money = 3
-		self.tableau = [] # all the players played cards
-		self.military = [] # war wins/losses
-		self.east_trade_prices = {
-			RESOURCE_WOOD: 2,
-			RESOURCE_ORE: 2,
-			RESOURCE_STONE: 2,
-			RESOURCE_BRICK: 2,
-			RESOURCE_GLASS: 2,
-			RESOURCE_LOOM: 2,
-			RESOURCE_PAPER: 2
-		}
-		self.west_trade_prices = self.east_trade_prices.copy()
-		self.wonder = None
-		self.personality = None
+    def __init__(self, name):
+        self.name = name
+        self.money = 3
+        self.board = []  # all the players played cards
+        self.military = []  # war wins/losses
+        self.east_trade_prices = {
+            RESOURCE_WOOD: 2,
+            RESOURCE_ORE: 2,
+            RESOURCE_STONE: 2,
+            RESOURCE_BRICK: 2,
+            RESOURCE_GLASS: 2,
+            RESOURCE_LOOM: 2,
+            RESOURCE_PAPER: 2,
+        }
+        self.west_trade_prices = self.east_trade_prices.copy()
+        self.wonder = None
+        self.personality = None
 
-	def set_personality(self, persona):
-		self.personality = persona
+    def set_personality(self, persona):
+        self.personality = persona
 
-	def get_name(self):
-		return self.name
+    def get_name(self):
+        return self.name
 
-	def get_cards(self):
-		return self.tableau
+    def get_cards(self):
+        return self.board
 
-	def play_hand(self, hand, west_player, east_player):
-		''' return the card and action done'''
-		options = []
-		for card in hand:
-			#print card.get_name(), self.is_card_in_tableau(card)
-			if not self.is_card_in_tableau(card):
-				if self.can_build_with_chain(card):
-					options.append((ACTION_PLAYCARD, card))
-				elif self.buy_card(card, west_player, east_player):
-					options.append((ACTION_PLAYCARD, card))
-			options.append((ACTION_DISCARD, card))
-			if False:#self.wonder.built_stages < 3: #FIXMEself.wonder.stages:
-				options.append((ACTION_STAGEWONDER, card))
-		i = 0
-		print("-=================-")
+    def play_hand(self, hand, west_player, east_player):
+        """return the card and action done"""
+        options = []
+        for card in hand:
+            # print card.get_name(), self.is_card_in_board(card)
+            if not self.is_card_in_board(card):
+                if self.can_build_with_chain(card):
+                    options.append((ACTION_PLAYCARD, card))
+                elif self.buy_card(card, west_player, east_player):
+                    options.append((ACTION_PLAYCARD, card))
+            options.append((ACTION_DISCARD, card))
+            if False:  # self.wonder.built_stages < 3: #FIXMEself.wonder.stages:
+                options.append((ACTION_STAGEWONDER, card))
+        i = 0
+        print("-=================-")
 
-		options = sorted(options, key=lambda x: {CARDS_GREY:0, CARDS_BROWN:1, CARDS_YELLOW:2, CARDS_BLUE:3, CARDS_RED:4, CARDS_GREEN:5, CARDS_PURPLE:6}[x[1].get_colour()])
-		for o in options:
-			actions = { ACTION_PLAYCARD:"Play", ACTION_DISCARD:"Discard", ACTION_STAGEWONDER:"Stage" }
-			card = o[1]
-			print("[%d]: %s\t%s\t%s" % (i, actions[o[0]], card.get_cost_as_string(), card.pretty_print_name()))
-			i += 1
-		print("-=================-")
+        options = sorted(
+            options,
+            key=lambda x: {
+                CARDS_GREY: 0,
+                CARDS_BROWN: 1,
+                CARDS_YELLOW: 2,
+                CARDS_BLUE: 3,
+                CARDS_RED: 4,
+                CARDS_GREEN: 5,
+                CARDS_PURPLE: 6,
+            }[x[1].get_colour()],
+        )
+        for o in options:
+            actions = {
+                ACTION_PLAYCARD: "Play",
+                ACTION_DISCARD: "Discard",
+                ACTION_STAGEWONDER: "Stage",
+            }
+            card = o[1]
+            print(
+                "[%d]: %s\t%s\t%s"
+                % (
+                    i,
+                    actions[o[0]],
+                    card.get_cost_as_string(),
+                    card.pretty_print_name(),
+                )
+            )
+            i += 1
+        print("-=================-")
 
-		return options[self.personality.make_choice(options)]
+        return options[self.personality.make_choice(options)]
 
+    def set_wonder(self, wonder):
+        self.wonder = wonder
 
-	def set_wonder(self, wonder):
-		self.wonder = wonder
+    def is_card_in_board(self, card):
+        return find_card(self.get_cards(), card.get_name()) != None
 
-	def is_card_in_tableau(self, card):
-		return find_card(self.get_cards(), card.get_name()) != None
+    def can_build_with_chain(self, card):
+        for precard in card.prechains:
+            if find_card(self.get_cards(), precard):
+                return True
+        return False
 
-	def can_build_with_chain(self, card):
-		for precard in card.prechains:
-			if find_card(self.get_cards(), precard):
-				return True
-		return False
+    def buy_card(self, card, west_player, east_player):
+        missing = []
+        money_spent = 0
+        trade_east = 0
+        trade_west = 0
+        options = []
+        if len(card.cost) == 0:
+            return [CardPurchaseOption([], 0, [], [])]
+        for i in range(len(card.cost)):
+            cost = deque(card.cost)
+            cost.rotate(i)
+            for east_first in [True, False]:
+                x = self._find_resource_cards(
+                    list(cost),
+                    west_player.get_cards(),
+                    east_player.get_cards(),
+                    east_first,
+                )
+                if x and x not in options:
+                    options.append(x)
+        # we now remove any of the options which we cant afford to pay for trades
+        legal_options = []
+        for o in options:
+            cost = o.coins
+            for c in o.east_trades:
+                o.east_cost = self.east_trade_prices[c.resource] * c.count
+                cost += o.east_cost
+            for c in o.west_trades:
+                o.west_cost = self.west_trade_prices[c.resource] * c.count
+                cost += o.west_cost
+            if cost <= self.money:
+                o.set_total(cost)
+                legal_options.append(o)
+            # Setting the total cost is buggy
+        # print sorted(legal_options, key=lambda x: x.total_cost)
+        return sorted(legal_options, key=lambda x: x.total_cost)
 
-	def buy_card(self, card, west_player, east_player):
-		missing = []
-		money_spent = 0
-		trade_east = 0
-		trade_west = 0
-		options = []
-		if len(card.cost) == 0:
-			return [CardPurchaseOption([], 0, [], [])]
-		for i in range(len(card.cost)):
-			cost = deque(card.cost)
-			cost.rotate(i)
-			for east_first in [True, False]:
-				x = self._find_resource_cards(list(cost), west_player.get_cards(), east_player.get_cards(), east_first)
-				if x and x not in options:
-					options.append(x)
-		# we now remove any of the options which we cant afford to pay for trades
-		legal_options = []
-		for o in options:
-			cost = o.coins
-			for c in o.east_trades:
-				o.east_cost = self.east_trade_prices[c.resource] * c.count
-				cost += o.east_cost
-			for c in o.west_trades:
-				o.west_cost = self.west_trade_prices[c.resource] * c.count
-				cost += o.west_cost
-			if cost <= self.money:
-				o.set_total(cost)
-				legal_options.append(o)
-			# Setting the total cost is buggy
-		#print sorted(legal_options, key=lambda x: x.total_cost)
-		return sorted(legal_options, key=lambda x: x.total_cost)
+    def _find_resource_cards(
+        self, needed_resources, west_cards, east_cards, east_first=True
+    ):
+        def __is_card_used(card, used_cards_array):
+            for x in used_cards_array:
+                if card == x.card:
+                    return True
+            return False
 
-	def _find_resource_cards(self, needed_resources, west_cards, east_cards, east_first=True):
-		def __is_card_used(card, used_cards_array):
-			for x in used_cards_array:
-				if card == x.card:
-					return True
-			return False
-		def __check_tableau(r, tableau, used_cards, tradeable_only):
-			for c in tableau: # FIXME: WONDER too
-				if not __is_card_used(c, used_cards):
-					is_resource, tradeable = c.is_resource_card()
-					if is_resource and ((not tradeable_only) or (tradeable_only == tradeable)):
-						count = c.provides_resource(r)
-						if count == 0:
-							continue
-						return (c, count)
-			return (None, 0)
+        def __check_board(r, board, used_cards, tradeable_only):
+            for c in board:  # FIXME: WONDER too
+                if not __is_card_used(c, used_cards):
+                    is_resource, tradeable = c.is_resource_card()
+                    if is_resource and (
+                        (not tradeable_only) or (tradeable_only == tradeable)
+                    ):
+                        count = c.provides_resource(r)
+                        if count == 0:
+                            continue
+                        return (c, count)
+            return (None, 0)
 
-		used_cards = []
-		coins = 0
-		east_trades = []
-		west_trades = []
-		card_sets = [(self.get_cards(), used_cards, False)]
-		if east_first:
-			card_sets += [(east_cards, east_trades, True), (west_cards, west_trades, True)]
-		else:
-			card_sets += [(west_cards, west_trades, True), (east_cards, east_trades, True)]
+        used_cards = []
+        coins = 0
+        east_trades = []
+        west_trades = []
+        card_sets = [(self.get_cards(), used_cards, False)]
+        if east_first:
+            card_sets += [
+                (east_cards, east_trades, True),
+                (west_cards, west_trades, True),
+            ]
+        else:
+            card_sets += [
+                (west_cards, west_trades, True),
+                (east_cards, east_trades, True),
+            ]
 
-		while len(needed_resources):
-			r = needed_resources[0]
-			found = False
-			if r == RESOURCE_MONEY:
-				coins += 1
-				needed_resources.remove(r)
-				continue
-			for cards, used, tradeable_only in card_sets:
-				card, count = __check_tableau(r, cards, used, tradeable_only)
-				if card and count > 0:
-					found = True
-					needed_count = 0
-					for i in range(0, count):
-						if r not in needed_resources:
-							break
-						needed_count += 1
-						needed_resources.remove(r)
-					used.append(CardPurchaseUse(card, r, needed_count))
-					break
-			if not found:
-				return None
-		return CardPurchaseOption(used_cards, coins, west_trades, east_trades)
+        while len(needed_resources):
+            r = needed_resources[0]
+            found = False
+            if r == RESOURCE_MONEY:
+                coins += 1
+                needed_resources.remove(r)
+                continue
+            for cards, used, tradeable_only in card_sets:
+                card, count = __check_board(r, cards, used, tradeable_only)
+                if card and count > 0:
+                    found = True
+                    needed_count = 0
+                    for i in range(0, count):
+                        if r not in needed_resources:
+                            break
+                        needed_count += 1
+                        needed_resources.remove(r)
+                    used.append(CardPurchaseUse(card, r, needed_count))
+                    break
+            if not found:
+                return None
+        return CardPurchaseOption(used_cards, coins, west_trades, east_trades)
+
 
 class CardPurchaseUse:
-	def __init__(self, card, resource, count):
-		self.card = card
-		self.resource = resource
-		self.count = count
+    def __init__(self, card, resource, count):
+        self.card = card
+        self.resource = resource
+        self.count = count
 
-	def __eq__(self, other):
-		return self.resource == other.resource \
-			and self.count == other.count \
-			and self.card.get_name() == other.card.get_name()
+    def __eq__(self, other):
+        return (
+            self.resource == other.resource
+            and self.count == other.count
+            and self.card.get_name() == other.card.get_name()
+        )
 
-	def __repr__(self):
-		if len(self.card.get_info()) == 1:
-			return "%s"% (self.card)
-		return "%s -> %s * %d" % (self.card, self.resource, self.count)
+    def __repr__(self):
+        if len(self.card.get_info()) == 1:
+            return "%s" % (self.card)
+        return "%s -> %s * %d" % (self.card, self.resource, self.count)
+
 
 class CardPurchaseOption:
-	def __init__(self, cards, coins, west_trades, east_trades):
-		self.cards = cards
-		self.coins = coins
-		self.west_trades = west_trades
-		self.east_trades = east_trades
-		self.total_cost = 0
-		self.east_cost = 0
-		self.west_cost = 0
+    def __init__(self, cards, coins, west_trades, east_trades):
+        self.cards = cards
+        self.coins = coins
+        self.west_trades = west_trades
+        self.east_trades = east_trades
+        self.total_cost = 0
+        self.east_cost = 0
+        self.west_cost = 0
 
-	def set_total(self, cost):
-		self.total_cost = cost
+    def set_total(self, cost):
+        self.total_cost = cost
 
-	def __eq__(self, other):
-		if self.coins != other.coins:
-			return False
-		for us, them in [(self.cards, other.cards), \
-						(self.west_trades, other.west_trades), \
-						(self.east_trades, other.east_trades)]:
-			if len(us) != len(them):
-				return False
-			for x in us:
-				if x not in them:
-					return False
+    def __eq__(self, other):
+        if self.coins != other.coins:
+            return False
+        for us, them in [
+            (self.cards, other.cards),
+            (self.west_trades, other.west_trades),
+            (self.east_trades, other.east_trades),
+        ]:
+            if len(us) != len(them):
+                return False
+            for x in us:
+                if x not in them:
+                    return False
 
-		return True
+        return True
 
-	def __repr__(self):
-		return "{ (total: $%d)\n\t%s\n\t$%d\n\tWEST:%s\n\tEAST:%s\n}" % (self.total_cost, self.cards, self.coins, self.west_trades, self.east_trades)
+    def __repr__(self):
+        return "{ (total: $%d)\n\t%s\n\t$%d\n\tWEST:%s\n\tEAST:%s\n}" % (
+            self.total_cost,
+            self.cards,
+            self.coins,
+            self.west_trades,
+            self.east_trades,
+        )
