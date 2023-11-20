7c283fffe7fc9fe2d9991550bb5677bc8fa0a2df
koen <koen@oostermeijer.net>
2023-11-20 22:22:53 +0000

    remove comments

--- players/Personalities.py
+++ players/Personalities.py
@@ -1,15 +1,3 @@
-#!/usr/bin/python
-#
-# Copyright 2015 - Jonathan Gordon
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
-# KIND, either express or implied.
-
 from sys import stdin
 from common import *
 import cards
@@ -18,7 +6,7 @@ import cards
 class Personality:
 	def __init__(self):
 		pass
-	
+
 	def make_choice(self, options):
 		pass
 
@@ -26,14 +14,14 @@ class Personality:
 class StupidAI(Personality):
 	def __init__(self):
 		pass
-	
+
 	def make_choice(self, options):
 		return 0
 
 class Human(Personality):
 	def __init__(self):
 		pass
-	
+
 	def make_choice(self, options):
 		return int(stdin.readline())
 
